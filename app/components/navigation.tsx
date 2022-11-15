a4b5313008eab2da5b66bc0e2de601da24f1527e
mafh612@gmail.com <mafh612@gmail.com>
2022-11-15 14:56:34 +0100

    fix(input): menu

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Box, Button, Stack, ThemeProvider, Typography, createTheme } from '@mui/material'
 import { AccountCircleOutlined, Home as HomeIcon } from '@mui/icons-material'
-import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom'
+import { Link as RouterLink, LinkProps as RouterLinkProps, useLocation } from 'react-router-dom'
 
 const LinkBehavior = React.forwardRef<HTMLAnchorElement, Omit<RouterLinkProps, 'to'> & { href: RouterLinkProps['to'] }>(
   ({ href, ...other }, ref) => <RouterLink ref={ref} to={href} {...other} />
@@ -18,14 +18,17 @@ export const theme = createTheme({
 })
 
 export const Navigation = () => {
+  const location = useLocation()
+  const active = (label: string): 'contained' | 'outlined' => (location.pathname === label ? 'contained' : 'outlined')
+
   return (
     <ThemeProvider theme={theme}>
       <Box marginBottom={2}>
         <Stack direction='row' justifyContent='center' alignItems='center' spacing={2}>
-          <Button href='/' variant='contained' startIcon={<HomeIcon />}>
+          <Button href='/' variant={active('/')} startIcon={<HomeIcon />}>
             <Typography>Home</Typography>
           </Button>
-          <Button href='/users' variant='outlined' startIcon={<AccountCircleOutlined />}>
+          <Button href='/users' variant={active('/users')} startIcon={<AccountCircleOutlined />}>
             <Typography>Users</Typography>
           </Button>
         </Stack>
